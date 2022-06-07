0977607e41dc8c8c30ad0b96a151e4e9ef65ab73
chagnzune <changzunelee@gmail.com>
2022-06-07 19:02:07 +0900

    Initial commit

---
+++ src/main/java/com/zercobase/WebSample/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.zercobase.WebSample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class SampleController{
+
+    @RequestMapping(value = "/order/1", method = RequestMethod.GET)
+public String getOrder() {
+    log.info("Get some order");
+    return "orderId:1, orderAmount:1000";
+
+}
+}
---
+++ src/test/java/com/zercobase/WebSample/WebSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zercobase.WebSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
